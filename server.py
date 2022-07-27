a31b88a1ca9dfebac5f57bcff909796654d7a8f2
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2022-07-27 13:59:29 +0300

    send request to server

--- server.py
+++ server.py
@@ -2,9 +2,8 @@ import requests
 
 
 
+res = requests.post("http://127.0.0.1:3000/api/login", json={"login": "test_login4", "password": 'test_password2'})
 
-#res = requests.get("http://127.0.0.1:3000/api/courses/2")
-
-res = requests.post("http://127.0.0.1:3000/api/courses/2", json={"login": "test_login2", "password": 'test_password2'})
+#res = requests.post("http://127.0.0.1:3000/api/reg", json={"login": "test_login4", "password": 'test_password2'})
 #print(res.json())
 print(res.json())
