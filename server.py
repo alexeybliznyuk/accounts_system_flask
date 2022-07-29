750c544ff16004a7b1a1480a2380de9185dcf055
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2022-07-29 11:33:40 +0300

    template for post request

--- server.py
+++ server.py
@@ -2,8 +2,8 @@ import requests
 
 
 
-res = requests.post("http://127.0.0.1:3000/api/login", json={"login": "test_login4", "password": 'test_password2'})
+#res = requests.post("http://127.0.0.1:3000/api/login", json={"login": "test_login4", "password": 'test_password2'})
 
 #res = requests.post("http://127.0.0.1:3000/api/reg", json={"login": "test_login4", "password": 'test_password2'})
 #print(res.json())
-print(res.json())
+#print(res.json())
