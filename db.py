5386cdc9963dee0d8d30bcb79e5fe7ed0fcdc9d8
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2022-07-29 11:30:50 +0300

    db
    add config

--- db.py
+++ db.py
@@ -2,17 +2,13 @@ import psycopg2
 
 from psycopg2 import Error
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-
-import requests
-import json
+from config import db_host, db_port, db_user, db_password
 
 
-login_col = 'login'
-password_col = 'password'
+
+
 table_name = 'users'
-#login_request = 'test_login2'
-#password_request = 'test_password3'
-#id_request = "1"
+
 
 
 
@@ -21,11 +17,11 @@ def connect_cursor():
     global connection, cursor
     try:
 
-        connection = psycopg2.connect(user="postgres",
+        connection = psycopg2.connect(user=db_user,
 
-                                    password="qwerty",
-                                    host="localhost",
-                                    port="5433")
+                                    password=db_password,
+                                    host=db_host,
+                                    port=db_port)
         connection.autocommit = True
         cursor = connection.cursor()
         cursor.execute(
@@ -45,7 +41,7 @@ def connect_cursor():
 
 
 
-
+#отключение сервера
 def close_connect(cursor, connection):
 
     cursor.close()
@@ -56,33 +52,13 @@ def close_connect(cursor, connection):
 
 
 
+#получение пароля  по логину из дб
+def get_by_login(cursor, login_request):
 
-
-    #test insert
-def test_insert(cursor):
     cursor.execute(
-        f"""INSERT INTO users ({login_col}, {password_col}) VALUES
-        ('{login_request}', '{password_request}');"""
+        f"""SELECT password FROM {table_name} WHERE login = '{login_request}';"""
     )
-    print("[INFO] Data was succefully inserted")
-
-
-
-#def test_get(cursor):
- #   cursor.execute(
-#        """SELECT password FROM users WHERE login = 'test_login2';"""
-#    )
- #   print(cursor.fetchone())
-
-
-def test_get(cursor, login_request):
- #   global password_col, table_name, login_col, login_reqest
-    cursor.execute(
-        f"""SELECT {password_col} FROM {table_name} WHERE {login_col} = '{login_request}';"""
-    )
- #   answer = {'login': login_request, 'password':cursor.fetchone()[0]}
-
- #   return answer
+    
     cursor_fetch = cursor.fetchone()
     if cursor_fetch == None:
 
@@ -94,25 +70,23 @@ def test_get(cursor, login_request):
 
     else:
         cursor.execute(
-            f"""SELECT id FROM {table_name} WHERE {login_col} = '{login_request}';"""
+            f"""SELECT id FROM {table_name} WHERE login = '{login_request}';"""
         )
         account_id = cursor.fetchone()
         answer = {'login': login_request, 'password': cursor_fetch[0], 'id': account_id[0]}
         return answer
 
-#    else:
- #       answer = {'login': login_request, 'password': cursor.fetchone()}
- #       return answer
 
 
 
 
 
 
+#проверяет наличие в дб по логину для reg
 def check_presence(cursor, login):
- #   global password_col, table_name, login_col, login_reqest
+
     cursor.execute(
-        f"""SELECT id FROM {table_name} WHERE {login_col} = '{login}';"""
+        f"""SELECT id FROM {table_name} WHERE login = '{login}';"""
     )
     account_id = cursor.fetchone()
  #   print(account_id)
@@ -121,64 +95,26 @@ def check_presence(cursor, login):
     else:
         return "This login already used"
 
-
+#регистрация при удаче возвращает id, при неудаче "This login already used"
 def reg(cursor, login, password):
     presence = check_presence(cursor, login)
     if presence == "This login is free":
         cursor.execute(
-            f"""INSERT INTO users ({login_col}, {password_col}) VALUES
+            f"""INSERT INTO users (login, password) VALUES
             ('{login}', '{password}');"""
         )
         cursor.execute(
-            f"""SELECT id FROM {table_name} WHERE {login_col} = '{login}';"""
+            f"""SELECT id FROM {table_name} WHERE login = '{login}';"""
         )
         account_id = cursor.fetchone()
         return account_id[0]
     else:
         return presence
 
-#connect_cursor()
-#test_insert(cursor)
-#print(reg(cursor, login_request, password_request))
-#print(test_get(cursor, login_request))
-#print(str(type(test_get(cursor, 'test_login'))))
-#rint(json.dumps(test_get(cursor, 'test_login')))
-#close_connect(cursor, connection)
-
-
-#create table
-
- #   cursor.execute(
-  #      """CREATE TABLE users(
-   #         id serial PRIMARY KEY,
-   #         login varchar(50) NOT NULL,
-  #          password varchar(50) NOT NULL);"""
-  #  )
- #   connection.commit()
-    #   print("[INFO] Table created successfully")
 
 
 
 
-
-
-
-
-
-    #test insert
- #   cursor.execute(
-  #      """INSERT INTO users (login, password) VALUES
-   #     ('test_login', 'test_password');"""
-   # )
-   # print("[INFO] Data was succefully inserted")
-
-
-#test get reauest
-
-#    cursor.execute(
-#        """SELECT password FROM users WHERE login = 'test_login';"""
- #   )
- #   print(cursor.fetchone())
 
 
 
